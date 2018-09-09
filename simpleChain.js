a8da3df52ae0d93fe2e276977a94e69a6fc0bfa2
CDA <daemschris@gmail.com>
2018-09-09 21:50:28 +0200

    implemented required changes

--- simpleChain.js
+++ simpleChain.js
@@ -67,49 +67,59 @@ class Blockchain{
     async getBlock(blockHeight) {
         return JSON.parse(await this.getLevelDBData(blockHeight))
     }
+    
 
-    // validate block
     validateBlock(blockHeight){
-        // get block object
-        let block = this.getBlock(blockHeight);
-        // get block hash
-        let blockHash = block.hash;
-        // remove block hash to test block integrity
-        block.hash = '';
-        // generate block hash
-        let validBlockHash = SHA256(JSON.stringify(block)).toString();
-        // Compare
-        if (blockHash===validBlockHash) {
-            return true;
-          } else {
-            console.log('Block #'+blockHeight+' invalid hash:\n'+blockHash+'<>'+validBlockHash);
-            return false;
-          }
-      }
-  
+        return new Promise((resolve, reject) => {
+            this.getBlockHeight(blockHeight).then((block) => {
+                // get block hash
+                let blockHash = block.hash;
+                // remove block hash to test block integrity
+                block.hash = '';
+                // generate block hash
+                let validBlockHash = SHA256(JSON.stringify(block)).toString();
+                // Compare
+                if (blockHash === validBlockHash) {
+                    resolve(true);
+                } else {
+                    console.log('Block #'+blockHeight+' invalid hash:\n'+blockHash+'<>'+validBlockHash);
+                    reject(false);
+                }
+            }).catch((err) => {
+                reject(err);
+            })
+        })
+    }
+    
+    
      // Validate blockchain
-      validateChain(){
+    validateChain(){
         let errorLog = [];
 
         let height = this.getBlockHeight();
 
         for (var i = 0; i < height; i++) {
+          
           // validate block
-          if (!this.validateBlock(i))errorLog.push(i);
+          if (!this.validateBlock(i)) errorLog.push(i);
+
+          if ( i == height - 1) break;
           // compare blocks hash link
-          let blockHash = this.chain[i].hash;
-          let previousHash = this.chain[i+1].previousBlockHash;
-          if (blockHash!==previousHash) {
+          let blockHash = this.getBlock(i).hash;
+          let previousHash = this.getBlock(i + 1).previousBlockHash;
+          if (blockHash !== previousHash) {
             errorLog.push(i);
           }
         }
-        if (errorLog.length>0) {
-          console.log('Block errors = ' + errorLog.length);
-          console.log('Blocks: '+errorLog);
+        
+        if (errorLog.length > 0) {
+            console.log('Block errors = ' + errorLog.length);
+            console.log('Blocks: ' + errorLog);
         } else {
-          console.log('No errors detected');
+            console.log('No errors detected');
         }
-      }
+    }
+      
 
     /* ===== level DB  ================================
     |   persistent data functions 		         	   |
